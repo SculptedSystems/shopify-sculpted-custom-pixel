888df563b17f098b0d6c6762e64da810c17eb897
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-11-18 11:03:52 -0500

    enhance debug message clarity

--- src/utils/buildEventHandler.ts
+++ src/utils/buildEventHandler.ts
@@ -6,7 +6,7 @@ export function buildEventHandler<T extends AnalyticsEvent>(
   handler: AnalyticsEventHandler<T>,
 ): AnalyticsEventHandler {
   return (event: AnalyticsEvent) => {
-    logger.debug(stringifyObject(event));
+    logger.debug(`Event Data -> ${stringifyObject(event)}`);
     handler(event as T);
   };
 }

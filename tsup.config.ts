19cda52609dd4e4cc2e9f97d7f5090f6faad5ac4
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-11-19 11:42:24 -0500

    add minify (otherwise 1.4k+ locs)

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   outExtension: () => ({ js: ".js" }),
   outDir: "dist",
   target: "es2020",
-  minify: false,
+  minify: true,
   sourcemap: false,
   clean: true,
   treeshake: true,

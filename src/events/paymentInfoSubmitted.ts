23570da58489438475d4466014adff1d78c51349
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-11-18 20:46:34 -0500

    paymentInfoSubmitted event pushing for all platforms

--- src/events/paymentInfoSubmitted.ts
+++ src/events/paymentInfoSubmitted.ts
@@ -5,7 +5,11 @@ import { PixelEventsPaymentInfoSubmitted } from "@sculptedsystems/shopify-web-pi
 
 import { config } from "@config";
 
-import { getGoogleItemsFromShopifyCheckoutLineItems } from "@helpers/items";
+import {
+  getContentIdsFromShopifyCheckoutLineItems,
+  getGoogleItemsFromShopifyCheckoutLineItems,
+  getMetaContentsFromShopifyCheckoutLineItems,
+} from "@helpers/items";
 import { getWholeCartCouponFromDiscountApplications } from "@helpers/discount";
 
 import { buildEventHandler } from "@utils/buildEventHandler";
@@ -52,12 +56,60 @@ function prepareGooglePaymentInfoSubmitted(
   };
 }
 
+function prepareMetaPaymentInfoSubmitted(
+  event: PixelEventsPaymentInfoSubmitted,
+  message: DataLayerMessage,
+): void {
+  if (!config.platform.meta) {
+    return;
+  }
+
+  const eventData = event.data;
+  const checkout = eventData.checkout;
+
+  // parameter: content_ids
+  const content_ids = getContentIdsFromShopifyCheckoutLineItems(
+    checkout.lineItems,
+  );
+
+  // parameter: contents
+  const contents = getMetaContentsFromShopifyCheckoutLineItems(
+    checkout.lineItems,
+  );
+
+  // parameter: currency
+  const currency = checkout.subtotalPrice?.currencyCode;
+
+  // parameter: value
+  const value = checkout.subtotalPrice?.amount;
+
+  message.meta = {
+    event: "AddPaymentInfo",
+    content_ids: content_ids,
+    contents: contents,
+    currency: currency,
+    value: value,
+  };
+}
+
+function prepareTikTokPaymentInfoSubmitted(message: DataLayerMessage): void {
+  if (!config.platform.tiktok) {
+    return;
+  }
+
+  message.tiktok = {
+    event: "AddPaymentInfo",
+  };
+}
+
 function handlePaymentInfoSubmitted(
   event: PixelEventsPaymentInfoSubmitted,
 ): void {
   const message: DataLayerMessage = { event: "shopify_payment_info_submitted" };
 
   prepareGooglePaymentInfoSubmitted(event, message);
+  prepareMetaPaymentInfoSubmitted(event, message);
+  prepareTikTokPaymentInfoSubmitted(message);
 
   dataLayerPush(message);
 }

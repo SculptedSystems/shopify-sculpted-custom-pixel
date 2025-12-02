8e08bcf9832aaa4d074776c84dcfd005108b846a
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-12-02 18:11:56 -0500

    passing msgs to theme extension

--- src/utils/dataLayer.ts
+++ src/utils/dataLayer.ts
@@ -5,5 +5,6 @@ import { stringifyObject } from "@utils/stringify";
 export function dataLayerPush(message: DataLayerMessage): void {
   logger.info(`Pushing Message to Data Layer -> ${stringifyObject(message)}`);
 
-  window.dataLayer.push(message);
+  //window.dataLayer.push(message);
+  window.parent.postMessage(message, init.context.document.location.href);
 }

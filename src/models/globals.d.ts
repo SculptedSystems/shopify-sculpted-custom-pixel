73999b7fff264a34124652fa42a079af2a62921f
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2025-12-02 22:06:40 -0500

    change project name

--- src/models/globals.d.ts
+++ src/models/globals.d.ts
@@ -1,22 +1,14 @@
-// ================================================
-//   Global Declarations
-// ================================================
 import { Analytics, Browser, Init } from "@shopify/web-pixels-extension";
-
 import { DataLayer } from "@models";
 
 declare global {
-  // ============================
   // Shopify Web Pixels Runtime
-  // ============================
 
   const analytics: Analytics;
   const browser: Browser;
   const init: Init;
 
-  // ============================
   // Google Tag Manager Runtime
-  // ============================
 
   interface Window {
     dataLayer: DataLayer;
